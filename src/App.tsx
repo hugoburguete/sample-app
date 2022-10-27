e8b52cb1e0f99cee38d9ad0a1aed856c12714cce
Hugo Burguete <hugoburguete@hotmail.com>
2022-10-27 12:54:18 +0100

    wine PLP

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import myVariable from './playground';
-import Button from './Button/Button';
+
+interface WineObject {
+  id: number;
+  winery: string;
+  wine: string;
+  rating: {
+    average: string;
+    reviews: string;
+  };
+  location: string;
+  image: string;
+}
 
 function App() {
-  console.log('hello world');
+  const [results, setResults] = useState<WineObject[]>([]);
 
+  useEffect(() => {
+    const baseURL = 'https://api.sampleapis.com/wines/reds';
+    fetch(baseURL)
+      .then((resp) => resp.json())
+      .then((data) => setResults(data));
+  }, []);
+
+  // output the listing into the page.
   return (
-    <div className="App" aria-busy>
-      <Button text="asd" onPressed={() => {}} />
+    <div className="App">
+      <h1 className="text-center">My wines</h1>
+      <ul className="grid">
+        {results.map((wineObject) => (
+          <li
+            key={wineObject.id}
+            style={{ width: '100%', position: 'relative' }}
+          >
+            <div className="image-container">
+              <img src={wineObject.image} alt={wineObject.wine} />
+            </div>
+            <p>
+              <b>{wineObject.wine}</b>
+            </p>
+            <p>Location: {wineObject.location}</p>
+            <p>Rating: {wineObject.rating.average}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
